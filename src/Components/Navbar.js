66639bb946145c6baeb509190feb4c5d0e34dfb6
picapipicca <haewon09106@gmail.com>
2022-03-21 15:04:59 +0900

    scssVariables complete
    how to : $Name: property
    @import 'location';

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Title</h1>
+      <div className="box">
+          <h1>Another Title</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
